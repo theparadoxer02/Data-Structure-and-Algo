d45d888ad3d0efd05421e466e8c8f7402263a04c
abhimanyu98986@gmail.com <Mannu Gupta>
2017-02-12 21:34:10 +0530

    did something

--- Hackerrank/beautiful.py
+++
@@ -1,9 +0,0 @@
-i,j,k = [int(x) for x in input().strip().split(' ')]
-count = 0
-for t in range(i,j+1):
-	strg = str(t)
-	rev = int(strg[::-1])
-	if (((t-rev)//k) %2) == 0:
-		count = count + 1
-
-print(count)
